a7ad449116670a46f752ab7649fe1263a6487e95
Michael <mickel13@gmail.com>
2017-11-09 19:38:32 +1000

    gross implementation

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# cricket-cli
-Follow the cricket on your console
+# CRICKET-CLI
+
+A means of watching the cricket from the comfort of your terminal.
+
+### What
+
+
+### How
+
+1. Install
+
+    ```npm install```
+
+2. Run
+
+    ```node app.js```
+
+### Data
--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const request = require("request");
+const Table = require('cli-table');
+
 const apikey = "hNwe1emGGRRYKLkhj9OL9MuQl843";
+let unique_id;
+
+let table = new Table();
+
+// alternative API: https://cricscore-api.appspot.com/
 
 request.post({ 
     url: "http://cricapi.com/api/matches/", 
@@ -8,9 +15,38 @@ request.post({
     body = JSON.parse(body);
     for (let i = 0; i < body.matches.length; i++) {
         if (body.matches[i].matchStarted == true){
-            console.log(body.matches[i]);
+            let index = i.toString();
+            let game = body.matches[i]['team-2'] + " vs " + body.matches[i]['team-1'];
+            table.push({[index] : [game]}); 
         } else {
             break;
         }
     }
-});
+    
+    console.log(table.toString());
+    
+    prompt("Enter the match ID you would like to follow: ", function (input) {
+        unique_id = body.matches[input].unique_id;
+        console.log(unique_id);
+
+        request.post({
+            url: "http://cricapi.com/api/cricketScore/", 
+            form: { unique_id: unique_id, apikey: apikey }
+        }, function(err, resp, body) { 
+            console.log(body); 
+        });
+    });
+}); 
+
+
+function prompt(question, callback) {
+    let stdin = process.stdin,
+        stdout = process.stdout;
+
+    stdin.resume();
+    stdout.write(question);
+
+    stdin.once('data', function (data) {
+        callback(data.toString().trim());
+    });
+}
