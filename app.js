e24cff328004f297d2520a9602e2cd042a63255c
Michael <mickel13@gmail.com>
2017-11-16 20:56:53 +1000

    reworked for new API

--- app.js
+++ app.js
@@ -1,44 +1,67 @@
 const request = require("request");
 const Table = require('cli-table');
+const chalk = require('chalk');
+const CFonts = require('cfonts');
+const clear = require('clear');
 
-const apikey = "hNwe1emGGRRYKLkhj9OL9MuQl843";
-let unique_id;
+const APIurl = "http://cricscore-api.appspot.com/csa"; 
 
 let table = new Table();
 
-// alternative API: https://cricscore-api.appspot.com/
+CFonts.say('GARRY', {
+    font: 'block',         
+    align: 'left',         
+    colors: ['green'],     
+    background: 'black',   
+    letterSpacing: 1,      
+    lineHeight: 1,         
+    space: false,          
+    maxLength: '0'         
+});
 
-request.post({ 
-    url: "http://cricapi.com/api/matches/", 
-    form: { apikey: apikey }
+// Retrieve summary of games currently being played
+request.get({ 
+    url: APIurl,
+    json: true,
 }, function(err, resp, body) { 
-    body = JSON.parse(body);
-    for (let i = 0; i < body.matches.length; i++) {
-        if (body.matches[i].matchStarted == true){
-            let index = i.toString();
-            let game = body.matches[i]['team-2'] + " vs " + body.matches[i]['team-1'];
-            table.push({[index] : [game]}); 
-        } else {
-            break;
-        }
+
+    for (let i = 0; i < body.length; i++) {
+        let index = i.toString();
+        let game = body[i].t2 + " vs " + body[i].t1;
+        table.push({[index] : [game]});
     }
-    
+
     console.log(table.toString());
     
     prompt("Enter the match ID you would like to follow: ", function (input) {
-        unique_id = body.matches[input].unique_id;
-        console.log(unique_id);
+        let unique_id = body[input].id;
 
-        request.post({
-            url: "http://cricapi.com/api/cricketScore/", 
-            form: { unique_id: unique_id, apikey: apikey }
-        }, function(err, resp, body) { 
-            console.log(body); 
-        });
+        // Retrieve details for chosen game
+        request.get({
+            url: APIurl + "?id=" + unique_id,
+            json: true,
+        }, function(err, resp, body) {
+
+            // String manipulation...
+            let score = body[0].de.split("(")[0];
+            let results = body[0].de.split("(")[1].split(",")
+            let overs = results[0];
+            let batter1 = results[1];
+            let batter2 = results[2];
+            let bowler = body[0].de.split("(")[1].split(",")[3].split(")")[0];
+            let info = body[0].de.split("(")[1].split(",")[3].split(")")[1];
+            
+            // Clear the console & write ALL the things
+            clear();
+            console.log(chalk.blueBright(score + "(" + overs + ")" + info + "\n"));
+            console.log(batter1);
+            console.log(batter2);
+            console.log("\n" + bowler + "\n");
+        }); 
     });
 }); 
 
-
+// Simple function to get user input
 function prompt(question, callback) {
     let stdin = process.stdin,
         stdout = process.stdout;
